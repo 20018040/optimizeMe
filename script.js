78be92a4e3e0c750aa66a14dee50c00c122dfc01
20018040 <Jeff.kim01@student.csulb.edu>
2025-04-13 21:35:42 -0700

    addingCost

--- script.js
+++ script.js
@@ -19,4 +19,23 @@ inputs.forEach(input => {
     input.style.backgroundSize = 'cover';
     input.style.paddingRight = '30px';
   });
+  function mesoCost(level, currentStar){
+    divider = 20000;
+    if (currentStar < 10) {
+      divider = 2500;
+  } else if (currentStar == 10) {
+      divider = 40000;
+  } else if (currentStar == 11) {
+      divider = 22000;
+  } else if (currentStar == 12) {
+      divider = 15000;
+  } else if (currentStar == 13) {
+      divider = 11000;
+  } else if (currentStar == 14) {
+      divider = 7500;
+  } else if (currentStar == 15) {
+      divider = 22000;
+  }
+    return 100*Math.round(level**3 *  (currentStar+1)/divider + 10);  
+  }
 });
