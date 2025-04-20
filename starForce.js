10dbf90b75d02085b2772efa5d4f4185486dee02
20018040 <Jeff.kim01@student.csulb.edu>
2025-04-20 01:00:15 -0700

    more expectation

--- starForce.js
+++ starForce.js
@@ -1,7 +1,7 @@
 function mesoCost(level, currentStar){
     divider = 20000;
     if (currentStar < 10) {
-      divider = 2500;
+    return 100*Math.round((level**3) *  (currentStar+1)/2500 + 10);
   } else if (currentStar == 10) {
       divider = 40000;
   } else if (currentStar == 11) {
@@ -12,38 +12,36 @@ function mesoCost(level, currentStar){
       divider = 11000;
   } else if (currentStar == 14) {
       divider = 7500;
-  } else if (currentStar == 15) {
-      divider = 22000;
+  } 
+    return 100*Math.round((level**3) *  (currentStar+1)**2.7/divider + 10);  
   }
-    return 100*Math.round(level**3 *  (currentStar+1)/divider + 10);  
-  }
-  const preSaviorRates = {
-    0: [0.95, 0.05, 0, 0],
-    1: [0.9, 0.1, 0, 0],
-    2: [0.85, 0.15, 0, 0],
-    3: [0.85, 0.15, 0, 0],
-    4: [0.80, 0.2, 0, 0],
-    5: [0.75, 0.25, 0, 0],
-    6: [0.7, 0.3, 0, 0],
-    7: [0.65, 0.35, 0, 0],
-    8: [0.6, 0.4, 0, 0],
-    9: [0.55, 0.45, 0, 0],
-    10: [0.5, 0.5, 0, 0],
-    11: [0.45, 0, 0.55, 0],
-    12: [0.4, 0.0, 0.594, 0.006],
-    13: [0.35, 0.0, 0.637, 0.013],
-    14: [0.3, 0.0, 0.686, 0.014],
-    15: [0.3, 0.679, 0, 0.021],
-    16: [0.3, 0.0, 0.679, 0.021],
-    17: [0.3, 0.0, 0.679, 0.021],
-    18: [0.3, 0.0, 0.672, 0.028],
-    19: [0.3, 0.0, 0.672, 0.028],
-    20: [0.3, 0.63, 0, 0.07],
-    21: [0.3, 0, 0.63, 0.07],
-    22: [0.03, 0.0, 0.776, 0.194],
-    23: [0.02, 0.0, 0.686, 0.294],
-    24: [0.01, 0.0, 0.594, 0.396]
-  };
+//   const preSaviorRates = {
+//     0: [0.95, 0.05, 0, 0],
+//     1: [0.9, 0.1, 0, 0],
+//     2: [0.85, 0.15, 0, 0],
+//     3: [0.85, 0.15, 0, 0],
+//     4: [0.80, 0.2, 0, 0],
+//     5: [0.75, 0.25, 0, 0],
+//     6: [0.7, 0.3, 0, 0],
+//     7: [0.65, 0.35, 0, 0],
+//     8: [0.6, 0.4, 0, 0],
+//     9: [0.55, 0.45, 0, 0],
+//     10: [0.5, 0.5, 0, 0],
+//     11: [0.45, 0, 0.55, 0],
+//     12: [0.4, 0.0, 0.594, 0.006],
+//     13: [0.35, 0.0, 0.637, 0.013],
+//     14: [0.3, 0.0, 0.686, 0.014],
+//     15: [0.3, 0.679, 0, 0.021],
+//     16: [0.3, 0.0, 0.679, 0.021],
+//     17: [0.3, 0.0, 0.679, 0.021],
+//     18: [0.3, 0.0, 0.672, 0.028],
+//     19: [0.3, 0.0, 0.672, 0.028],
+//     20: [0.3, 0.63, 0, 0.07],
+//     21: [0.3, 0, 0.63, 0.07],
+//     22: [0.03, 0.0, 0.776, 0.194],
+//     23: [0.02, 0.0, 0.686, 0.294],
+//     24: [0.01, 0.0, 0.594, 0.396]
+//   };
   const starForceRates = {
     0:  [0.95, 0.05, 0.00, 0.00],
     1:  [0.90, 0.10, 0.00, 0.00],
@@ -65,51 +63,59 @@ function mesoCost(level, currentStar){
     17: [0.30, 0.00, 0.679, 0.021],
     18: [0.30, 0.00, 0.672, 0.028],
     19: [0.30, 0.00, 0.672, 0.028],
-    20: [0.30, 0.00, 0.63,  0.07],
+    20: [0.30, 0.63, 0.00,  0.07],
     21: [0.30, 0.00, 0.63, 0.07 ],
     22: [0.03, 0.00, 0.776, 0.194],
     23: [0.02, 0.00, 0.686, 0.294],
     24: [0.01, 0.00, 0.594, 0.396],
   };
   function starForce(level,currentStar,starCatch){
-  const r= Math.random();
-  const [successRate, maintainRate, dropRate, boomRate] = starForceRates[currentStar];
-  
-  if(r < successRate){
-    currentStar++;
-  }else if(r < successRate + maintainRate ){//maintain
-  
-  }else if(r < successRate + maintainRate +dropRate ){//drop
-    currentStar--;
-  }else{
-    currentStar = 12;
-    return [1,currentStar];
+    const r= Math.random();
+    const [successRate, maintainRate, dropRate, boomRate] = starForceRates[currentStar];
+    
+    if(r < successRate){
+        currentStar++;
+    }else if(r < successRate + maintainRate ){//maintain
+        currentStar = currentStar;
+    }else if(r < successRate + maintainRate +dropRate ){//drop
+        currentStar--;
+    }else if(r < successRate + maintainRate +dropRate + boomRate){
+        currentStar = 12;
+        return [1,currentStar];
+    }
+    return [0,currentStar]; 
   }
-  return [0,currentStar]; 
-  }
+  
   function calcExpected(attempts, level, currentStar, starCatch, safeGuard, goalStar){
     let totalCost = 0;
     let totalBoom = 0;
     for(let i = 0; i<attempts;i++){
       let trialStar = currentStar
       let sessionBoom = 0;
+      
       while(trialStar<goalStar){
         totalCost += mesoCost(level, trialStar);
+        // console.log("current star : ",trialStar);
         const [boom, newStar] = starForce(level, trialStar, starCatch);
-        sessionBoom += boom;
         trialStar = newStar;
+        if(boom ==1){
+            sessionBoom ++;
+        }
       }
+      console.log(sessionBoom + " session Boom End\n");
       totalBoom += sessionBoom;
     }
     
     console.log("Average Cost :", totalCost/attempts);
-    console.log("Average Boom :", totalBoom/attempts);
     
+    console.log("Total Boom :", totalBoom);
+    console.log("Average Boom :", totalBoom/attempts);
   }
+
   function main(){
     let level = 150;
     let currentStar = 12;
-    calcExpected(1000, 150, 12, false, false, 22);
+    calcExpected(1000, 150, 15, false, false, 22);
   }
   main()
   
