52268b81834d6b1406ed2b47ed3859de2c9e4e62
20018040 <Jeff.kim01@student.csulb.edu>
2025-05-24 11:20:35 -0700

    added imageMap

--- script.js
+++ script.js
@@ -139,16 +139,102 @@ function getValue(stat) {
   }
   return null; // or some default
 }
+
 const imageMap = {
-  'absolab helmet': 'absoHat.png',
-  'cra helmet': 'craHat.png',
-  'arcane helmet': 'arcaneHat.png',
+  // Ring
   'reinforced ring': 'reinforcedRing.png',
   'superior gollux ring': 'superiorGolluxRing.png',
   'meister ring': 'meisterRing.png',
   'endless terror': 'endlessTerror.png',
-  'kanna ring': 'kannaRing.png'
+  'kanna ring': 'kannaRing.png',
+
+  // Pocket
+  'pink holy cup': 'pinkHolyCup.png',
+  'cursed spell book': 'cursedSpellBook.png',
+
+  // Pendant
+  'dominator': 'dominator.png',
+  'superior gollux pendant': 'superiorGolluxPendant.png',
+  'source of suffering': 'sourceOfSuffering.png',
+
+  // Weapon
+  'arcane': 'arcaneWeapon.png',
+  'genesis': 'genesisWeapon.png',
+
+  // Belt
+  'golden clover belt': 'goldenCloverBelt.png',
+  'superior gollux belt': 'superiorGolluxBelt.png',
+  'dreamy belt': 'dreamyBelt.png',
+
+  // Hat
+  'cra helmet': 'craHat.png',
+  'absolab helmet': 'absoHat.png',
+  'arcane helmet': 'arcaneHat.png',
+
+  // Face Accessory
+  'condensed power crystal': 'condensedPowerCrystal.png',
+  'twilight mark': 'twilightMark.png',
+  'berserked': 'berserked.png',
+
+  // Eye Accessory
+  'aquatic letter eye': 'aquaticLetterEye.png',
+  'black bean mark': 'blackBeanMark.png',
+  'papulatus mark': 'papulatusMark.png',
+  'magic eye patch': 'magicEyePatch.png',
+
+  // Top
+  'cra top': 'craTop.png',
+  'absolab overall': 'absoOverall.png',
+  'arcane overall': 'arcaneOverall.png',
+  'eternal top': 'eternalTop.png',
+
+  // Bottom
+  'cra bottom': 'craBottom.png',
+  'eternal bottom': 'eternalBottom.png',
+
+  // Shoes
+  'pensalir shoes': 'pensalirShoes.png',
+  'ranmaru shoes': 'ranmaruShoes.png',
+  'absolab shoes': 'absolabShoes.png',
+  'arcane shoes': 'arcaneShoes.png',
+  'eternal shoes': 'eternalShoes.png',
+
+  // Earrings
+  'dea sidus earring': 'deaSidusEarring.png',
+  'reinforced gollux earring': 'reinforcedGolluxEarring.png',
+  'superior gollux earring': 'superiorGolluxEarring.png',
+  'command force earring': 'commandForceEarring.png',
+
+  // Shoulder
+  'royal black metal shoulder': 'royalBlackMetalShoulder.png',
+  'ranmaru shoulder': 'ranmaruShoulder.png',
+  'absolab shoulder': 'absolabShoulder.png',
+  'arcane shoulder': 'arcaneShoulder.png',
+  'eternal shoulder': 'eternalShoulder.png',
+
+  // Gloves
+  'pensalir gloves': 'pensalirGloves.png',
+  'ranmaru gloves': 'ranmaruGloves.png',
+  'absolab gloves': 'absolabGloves.png',
+  'arcane gloves': 'arcaneGloves.png',
+  'eternal gloves': 'eternalGloves.png',
+
+  // Emblem
+  'lvl 100 emblem': 'lvl100Emblem.png',
+  'mitra emblem': 'mitraEmblem.png',
+
+  // Secondary
+  'lvl 100 secondary': 'lvl100Secondary.png',
+  'pno secondary': 'pnoSecondary.png',
+
+  // Cape
+  'pensalir cape': 'pensalirCape.png',
+  'ranmaru cape': 'ranmaruCape.png',
+  'absolab cape': 'absolabCape.png',
+  'arcane cape': 'arcaneCape.png',
+  'eternal cape': 'eternalCape.png'
 };
+
 
 const ringDropdowns = document.querySelectorAll('.input1, .input2, .input3, .input4');
 const pocketDropdowns = document.querySelectorAll('.input5');
